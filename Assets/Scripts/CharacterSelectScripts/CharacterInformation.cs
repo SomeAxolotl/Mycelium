a0e5ccb60fea515fa37c5f0147390631d9fd7575
nhyde2000 <nolanhyde2000@gmail.com>
2023-10-06 13:53:58 -0400

    PurchaseButtonAdded

---
+++ Assets/Scripts/CharacterSelectScripts/CharacterInformation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class CharacterInformation 
+{
+  public string CharName;
+  public int index;
+  public int nutrientcost;
+  public bool isUnlocked;
+
+}
--- Assets/Scripts/CharacterSelectScripts/MenuManger.cs
+++ Assets/Scripts/CharacterSelectScripts/MenuManger.cs
@@ -1,15 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
 
 public class MenuManger : MonoBehaviour
 {
     public GameObject[] Characters;
     public int currentCharacterIndex;
+    public CharacterInformation[] Characterinfo;
+    public Button purchaseButton;
     void Start()
     {
+        foreach(CharacterInformation Char in Characterinfo)
+        {
+            if(Char.nutrientcost ==0)
+            {
+                Char.isUnlocked=true;
+            }
+            else
+            {
+                Char.isUnlocked = PlayerPrefs.GetInt(Char.CharName, 0)==0 ? false: true;
+            }
+        }
         currentCharacterIndex = PlayerPrefs.GetInt("SelectedChar", 0);
         foreach(GameObject Char in Characters)
         {
@@ -21,7 +36,7 @@ public class MenuManger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateUI();
     }
     public void ChangeNext()
     {
@@ -48,6 +63,26 @@ public class MenuManger : MonoBehaviour
             Characters[currentCharacterIndex].SetActive(true);
             PlayerPrefs.SetInt("SelectedChar", currentCharacterIndex);
         
+    }
+    public void UpdateUI()
+    {
+        CharacterInformation c = Characterinfo[currentCharacterIndex];
+        if (c.isUnlocked)
+        {
+            purchaseButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            purchaseButton.gameObject.SetActive(true);
+            if(c.nutrientcost< PlayerPrefs.GetInt("Nutrients", 0))
+            {
+                purchaseButton.interactable = true;
+            }
+            else
+            {
+                purchaseButton.interactable = false;
+            }
+        }
     }
     public void StartGame()
     {
