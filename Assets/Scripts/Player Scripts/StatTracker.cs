93bd6360ce029acd47cd76931823ef41c1b993fd
Dylan Hall <DylHall592@gmail.com>
2023-10-11 17:20:49 -0400

    Added Stat -> Blendshape Support

--- Assets/Scripts/Player Scripts/StatTracker.cs
+++ Assets/Scripts/Player Scripts/StatTracker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -35,6 +36,12 @@ public class StatTracker : MonoBehaviour
     float baseRegen = .05f;
     public float finalRegen;
 
+    //Blendshape Management
+    private int blendShapeCount = 4;
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    private Mesh skinnedMesh;
+    private List<float> statLevelWeights;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -42,6 +49,19 @@ public class StatTracker : MonoBehaviour
         speedLevel = 0;
         sentienceLevel = 0;
         vitalityLevel = 0;
+    }
+
+    void Awake()
+    {   
+        foreach (Transform child in this.transform)
+        {
+            if (child.tag == "playerCap")
+            {
+                skinnedMeshRenderer = child.gameObject.GetComponent<SkinnedMeshRenderer>();
+                skinnedMesh = child.gameObject.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+            }
+        }
+        
     }
 
     // Update is called once per frame
@@ -58,5 +78,77 @@ public class StatTracker : MonoBehaviour
         //Vitality Stat Calc
         maxHealth = baseHealth + (5f * vitalityLevel);
         finalRegen = baseRegen + (.05f * vitalityLevel);
+    }
+
+    int IncreaseStat(string statName, int increaseLevel)
+    {
+        if(String.Compare(statName, "primal") == 0)
+        {
+            if(primalLevel + increaseLevel >=15)
+            {
+                Debug.Log("Max Primal Level Reached!");
+                return 0;
+            }
+            primalLevel += increaseLevel;
+        }
+        else if(String.Compare(statName, "sentience") == 0)
+        {
+            if(sentienceLevel + increaseLevel >=15)
+            {
+                Debug.Log("Max Sentience Level Reached!");
+                return 0;
+            }
+            sentienceLevel += increaseLevel;
+        }
+            
+        else if(String.Compare(statName, "vitality") == 0)
+        {
+            if(vitalityLevel + increaseLevel >=15)
+            {
+                Debug.Log("Max Vitality Level Reached!");
+                return 0;
+            }
+            vitalityLevel += increaseLevel;
+        }
+        else if(String.Compare(statName, "speed") == 0)
+        {
+            if(speedLevel + increaseLevel >=15)
+            {
+                Debug.Log("Max Speed Level Reached!");
+                return 0;
+            }
+            speedLevel += increaseLevel;
+
+        }
+        else
+        {
+            Debug.Log("Stat Name does not exist in the current context. Stat options are \"primal\", \"sentience\", \"speed\", and \"vitality\"");
+            return 0;
+        }
+
+        UpdateBlendshape();
+        return 1;
+
+    }
+
+    void UpdateBlendshape()
+    {
+        float levelWeight = 0;
+        int totalPointsSpent = sentienceLevel + primalLevel + vitalityLevel + speedLevel;
+        //map the weight for Sentience
+        levelWeight = sentienceLevel/15f * totalPointsSpent/15f * 100;
+        statLevelWeights.Add(levelWeight);
+        //map the weight for Primal
+        levelWeight = primalLevel/15f * totalPointsSpent/15f * 100;
+        statLevelWeights.Add(levelWeight);
+        //map the weight for Vitality
+        levelWeight = vitalityLevel/15f * totalPointsSpent/15f * 100;
+        statLevelWeights.Add(levelWeight);
+        //map the weight for Speed
+        levelWeight = speedLevel/15f * totalPointsSpent/15f * 100;
+        statLevelWeights.Add(levelWeight);
+
+        for(int i=0;i<blendShapeCount;i++)
+            skinnedMeshRenderer.SetBlendShapeWeight(i,statLevelWeights[i]);
     }
 }
