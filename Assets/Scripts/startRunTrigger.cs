9027f7b04f0c194cf394e41ee94de339da07deef
Dylan Hall <DylHall592@gmail.com>
2023-10-21 16:00:36 -0400

    Minor Tweaks
    Renamed the script named DylanFucksAround with a professional name and professional functions, and added Spawnpoint functionality

--- Assets/Scripts/startRunTrigger.cs
+++ Assets/Scripts/startRunTrigger.cs
@@ -5,22 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class startRunTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Debug.Log("Start");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("HIT");
-
         if(other.CompareTag("currentPlayer"))
         {
             SceneManager.LoadScene("CharacterSelect");
