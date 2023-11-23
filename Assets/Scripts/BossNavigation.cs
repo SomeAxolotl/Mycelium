c33993e18d402731d549f30b79fd1424c85e73ad
TylerCook65 <tylercook6565@knights.ucf.edu>
2023-11-23 01:51:09 -0500

    boss rotation and movement pretty much done

--- Assets/Scripts/BossNavigation.cs
+++ Assets/Scripts/BossNavigation.cs
@@ -10,6 +10,8 @@ public class BossNavigation : MonoBehaviour
     private NavMeshAgent activeAgent;
     public GameObject meleeSide;
     public GameObject rangedSide;
+    private GameObject activeSide;
+    private GameObject inactiveSide;
     private Collider[] playerColliders;
     public LayerMask playerLayer;
     public LayerMask obstacleLayer;
@@ -22,43 +24,48 @@ public class BossNavigation : MonoBehaviour
     void Start()
     {
         activeAgent = meleeAgent;
+        activeSide = meleeSide;
+        inactiveSide = rangedSide;
+        InvokeRepeating("ChangeSides", 15f, 15f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerColliders = Physics.OverlapSphere(transform.position, detectionRange, playerLayer);
+        playerColliders = Physics.OverlapSphere(activeSide.transform.position, detectionRange, playerLayer);
         foreach (var playerCollider in playerColliders)
         {
             player = playerCollider.transform;
-            Vector3 dirToPlayer = (player.position - transform.position).normalized;
-            float dstToPlayer = Vector3.Distance(transform.position, player.position);
+            Vector3 dirToPlayer = (player.position - activeSide.transform.position).normalized;
+            float dstToPlayer = Vector3.Distance(activeSide.transform.position, player.position);
             var newRotation = Quaternion.LookRotation(dirToPlayer);
 
-            if (Vector3.Angle(transform.forward, dirToPlayer) < fieldOfView && !Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleLayer) ||
-                dstToPlayer <= (activeAgent.stoppingDistance + detectionBuffer) && !Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleLayer))
+            if (Vector3.Angle(transform.forward, dirToPlayer) < fieldOfView && !Physics.Raycast(activeSide.transform.position, dirToPlayer, dstToPlayer, obstacleLayer) ||
+                dstToPlayer <= (activeAgent.stoppingDistance + detectionBuffer) && !Physics.Raycast(activeSide.transform.position, dirToPlayer, dstToPlayer, obstacleLayer))
             {
                 if (canChase)
                 {
                     StartCoroutine(ChasePlayer());
                 }
 
+                activeSide.transform.rotation = Quaternion.Slerp(activeSide.transform.rotation, newRotation, Time.deltaTime * 10f);
+
                 //Keeps childrens position back to back
                 Vector3 relativePosition = new Vector3(0f, 0f, -1f);
-                rangedSide.transform.localPosition = meleeSide.transform.TransformPoint(relativePosition) - transform.position;
+                inactiveSide.transform.localPosition = activeSide.transform.TransformPoint(relativePosition) - transform.position;
 
                 //Keeps childrens rotation back to back
-                Vector3 directiontoBack = (meleeSide.transform.position - rangedSide.transform.position).normalized;
+                Vector3 directiontoBack = (activeSide.transform.position - inactiveSide.transform.position).normalized;
                 Quaternion lookRotation = Quaternion.LookRotation(-directiontoBack, Vector3.up);
-                rangedSide.transform.rotation = lookRotation;
+                inactiveSide.transform.rotation = lookRotation;
 
                 if (activeAgent == meleeAgent)
                 {
-
+                    activeAgent.speed = 5f;
                 }
                 else if(activeAgent == rangedAgent)
-                { 
-                    
+                {
+                    activeAgent.speed = 2f;
                 }
             }
         }
@@ -67,7 +74,22 @@ public class BossNavigation : MonoBehaviour
     {
         canChase = false;
         activeAgent.SetDestination(player.position);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.3f);
         canChase = true;
+    }
+    private void ChangeSides()
+    {
+        if(activeSide == meleeSide)
+        {
+            activeSide = rangedSide;
+            inactiveSide = meleeSide;
+            activeAgent = rangedAgent;
+        }
+        else if(activeSide == rangedSide)
+        {
+            activeSide = meleeSide;
+            inactiveSide = rangedSide;
+            activeAgent = meleeAgent;
+        }
     }
 }
