94839de02a9c2d53f17033defc772f20436035f5
flgina <71128309+flgina@users.noreply.github.com>
2023-11-26 22:26:04 -0500

    update
    working on leeching spore

--- Assets/Scripts/Skills/Vitality/LeechingSpore.cs
+++ Assets/Scripts/Skills/Vitality/LeechingSpore.cs
@@ -5,11 +5,38 @@ using UnityEngine;
 public class LeechingSpore : Skill
 {
     //Skill specific fields
+    /* damagee = 5
+    Duration = 5 sec
+    Range = 5
+    nearest enemy im 5m range spore gets attached
+    drains enemy for 5 seconds at a damage of 5 per second
+    heals player for 5 seonds at 5 HP per second*/
+    [SerializeField] private float smallRadius = 5f;
 
     public override void DoSkill()
     {
         //Skill specific stuff
 
+        DoLeech();
+
         EndSkill();
+    }
+
+    public void DoLeech()
+    {
+        int enemyLayerMask = 1 << LayerMask.NameToLayer("Enemy");
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, smallRadius, enemyLayerMask);
+        
+        finalSkillValue = 5;
+        foreach (Collider collider in colliders)
+        {
+            float distanceToCollider = Vector3.Distance(transform.position, collider.transform.position);
+
+            if (distanceToCollider >= smallRadius)
+            {
+                
+            }
+        }
     }
 }
