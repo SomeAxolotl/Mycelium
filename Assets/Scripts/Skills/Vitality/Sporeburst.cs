c10ec810edbe1d0e113e7aafa6476f3d71df898c
Shawn <lilpez2000@gmail.com>
2023-12-01 08:45:54 -0500

    Death Blossom and Sporeburst

--- Assets/Scripts/Skills/Vitality/Sporeburst.cs
+++ Assets/Scripts/Skills/Vitality/Sporeburst.cs
@@ -1,15 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Sporeburst : Skill
 {
-    //Skill specific fields
+    [SerializeField] private float burstRadius = 3f;
 
     public override void DoSkill()
     {
-        //Skill specific stuff
-
+        DamageEnemies();
         EndSkill();
     }
+
+    void DamageEnemies()
+    {
+        int enemyLayerMask = 1 << LayerMask.NameToLayer("Enemy");
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, burstRadius, enemyLayerMask);
+
+        float damage = finalSkillValue;
+        foreach (Collider collider in colliders)
+        {
+            NewEnemyHealth enemyHealth = collider.gameObject.GetComponent<NewEnemyHealth>();
+            if (enemyHealth != null)
+            {
+                NavMeshAgent navMeshAgent = collider.gameObject.GetComponent<NavMeshAgent>();
+                if (navMeshAgent != null)
+                {
+                    navMeshAgent.enabled = false;
+                    StartCoroutine(ReactivateNavMeshAfterDelay(navMeshAgent, 2f));
+                }
+                float healingAmount = 0.5f * damage;
+                enemyHealth.EnemyTakeDamage(damage);
+                HealPlayer(healingAmount);
+                Debug.Log("Sporeburst hit!");
+            }
+        }
+    }
+    void HealPlayer(float healingAmount)
+    {
+        NewPlayerHealth playerHealth = player.GetComponent<NewPlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.PlayerHeal(healingAmount);
+        }
+    }
+
+    IEnumerator ReactivateNavMeshAfterDelay(NavMeshAgent navMeshAgent, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (navMeshAgent != null)
+        {
+            navMeshAgent.enabled = true;
+        }
+    }
 }
+
