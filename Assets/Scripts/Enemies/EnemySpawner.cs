4a69e7ae6ffe9c0c778aef7bc2197be7dff5ae00
Dylan Hall <DylHall592@gmail.com>
2024-01-30 15:49:55 -0500

    Enemy Spawner Update
    Now includes multiple potential spawns, and a range they will be spawned in

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,32 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.EditorTools;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    [SerializeField] private List<EnemySpawn> EnemyList;
     public float spawnInterval = 2f;
-    public int maxEnemies = 5;
-    public int enemiesSpawnedLimit = 20;
+    [SerializeField][Tooltip("The radius around the EnemySpawner that the Enemy will randomly spawn in")][Min(0)] private float spawnRange = 0.4f;
+    [SerializeField][Tooltip("Functions the same as enemiesSpawnedLimit???")] private int maxEnemies = 5;
+    [SerializeField][Tooltip("The number of enemies spawned before this spawner destructs")] private int enemiesSpawnedLimit = 20;
     private int enemiesSpawnedCount = 0;
     private List<GameObject> spawnedEnemies = new List<GameObject>();
-    public bool spawnAll = false;
+    [SerializeField][Tooltip("Spawns the enemiesSpawnedLimit at once")] private bool spawnAll = false;
     private bool hasSpawned = false;
-    [SerializeField] private bool spawnOnStart = false;
-    private void Start()
+    [SerializeField][Tooltip("Spawns enemies on Start()")] private bool spawnOnStart = false;
+    private int totalEnemyWeight = 0;
+    
+    private List<GameObject> weightedEnemyList = new List<GameObject>();
+    void Start()
     {
-       if(!hasSpawned && spawnOnStart)
+        //Fills a list of potential enemy spawns with amounts based on their weight
+        foreach(EnemySpawn enemySpawn in EnemyList)
         {
-            if(spawnAll)
-            {
-                SpawnAllEnemies();
-                Destroy(gameObject);
-            }
-            else
-            {
-                StartCoroutine(SpawnEnemies());
-            }
+            totalEnemyWeight += enemySpawn.weight;
+            for(int i=0; i<enemySpawn.weight; i++)
+                weightedEnemyList.Add(enemySpawn.EnemyPrefab);
         }
+
+        if(!hasSpawned && spawnOnStart)
+            {
+                if(spawnAll)
+                {
+                    SpawnAllEnemies();
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    StartCoroutine(SpawnEnemies());
+                }
+            }
     }
 
     IEnumerator SpawnEnemies()
@@ -51,8 +66,8 @@ public class EnemySpawner : MonoBehaviour
 
     void SpawnEnemy()
     {
-        GameObject newEnemy= Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-
+        //Selects a random enemy from the weightedEnemyList, and spawns it
+        GameObject newEnemy = Instantiate(weightedEnemyList[UnityEngine.Random.Range(0,weightedEnemyList.Count-1)], new Vector3(transform.position.x + UnityEngine.Random.Range(-spawnRange, spawnRange), transform.position.y, transform.position.z + UnityEngine.Random.Range(-spawnRange, spawnRange)), Quaternion.identity);
         spawnedEnemies.Add(newEnemy);
     }
 
@@ -82,5 +97,18 @@ public class EnemySpawner : MonoBehaviour
                 }
             }            
         }
-    }   
+    }
 }
+
+//Custom Class that holds all information about what enemy to spawn
+[System.Serializable]
+class EnemySpawn
+{
+    [Tooltip("The label of this EnemySpawn in the inspector.")]
+    public string name = "Enemy";
+    [Tooltip("The prefab of the enemy to spawn")]
+    public GameObject EnemyPrefab = null;
+    
+    [Range(0,10)][Tooltip("The spawn weight of an enemy. The higher the weight compared to other enemies, the more likely the enemy will spawn.")]
+    public int weight = 1;
+} 
