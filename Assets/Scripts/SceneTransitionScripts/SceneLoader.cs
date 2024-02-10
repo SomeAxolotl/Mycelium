636b33788dc1e637857aa501546029f491550e9d
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-02-10 17:46:43 -0500

    Remade SceneLoader

--- Assets/Scripts/SceneTransitionScripts/SceneLoader.cs
+++ Assets/Scripts/SceneTransitionScripts/SceneLoader.cs
@@ -6,49 +6,125 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] public CanvasGroup enterLevelTransition;
+    [SerializeField] public CanvasGroup goodExitLevelTransition;
+    [SerializeField] public CanvasGroup badExitLevelTransition;
 
-    [SerializeField] public Animator enterLevelTransition;
-    [SerializeField] public Animator goodExitLevelTransition;
-    [SerializeField] public Animator badExitLevelTransition;
-
-    [SerializeField] public float transitionTime;
+    float defaultTransitionTime = 1f;
 
     private void Start()
     {
-        enterLevelTransition.SetTrigger("End");
+        StartCoroutine(EnterScene(2f));
     }
 
     public void BeginLoadScene(int sceneIndex, bool doGoodTransition)
     {
-        if(doGoodTransition == true)
+        if (doGoodTransition == true)
         {
-            StartCoroutine(LoadSceneGood(sceneIndex));
+            StartCoroutine(LoadSceneGood(sceneIndex, defaultTransitionTime));
         }
         else
         {
-            StartCoroutine(LoadSceneBad(sceneIndex));
+            StartCoroutine(LoadSceneBad(sceneIndex, defaultTransitionTime));
         }
     }
 
-    IEnumerator LoadSceneGood(int sceneIndex)
+    public void BeginLoadScene(int sceneIndex, bool doGoodTransition, float transitionTime)
     {
-        //Play animation
-        goodExitLevelTransition.SetTrigger("Start");
+        if (doGoodTransition == true)
+        {
+            StartCoroutine(LoadSceneGood(sceneIndex, transitionTime));
+        }
+        else
+        {
+            StartCoroutine(LoadSceneBad(sceneIndex, transitionTime));
+        }
+    }
 
-        //Wait
-        yield return new WaitForSecondsRealtime(transitionTime);
+    IEnumerator EnterScene(float transitionTime)
+    {
+        float elapsedTime = 0f;
+        float t = 0f;
+        float i = 0f;
+
+        enterLevelTransition.alpha = 1f;
+        enterLevelTransition.blocksRaycasts = true;
+        enterLevelTransition.interactable = true;
+
+        while (i < 0.5f)
+        {
+            i += Time.deltaTime;
+
+            yield return null;
+        }
+
+        while (elapsedTime < transitionTime + 0.5f)
+        {
+            t = elapsedTime / transitionTime;
+
+            enterLevelTransition.alpha = Mathf.Lerp(1f, 0f, t);
+
+            elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        enterLevelTransition.blocksRaycasts = false;
+        enterLevelTransition.interactable = false;
+        Debug.Log("Canvas Faded Out");
+    }
+
+    IEnumerator LoadSceneGood(int sceneIndex, float transitionTime)
+    {
+        float elapsedTime = 0f;
+        float t = 0f;
+
+        enterLevelTransition.blocksRaycasts = false;
+        enterLevelTransition.interactable = false;
+
+        while (elapsedTime < transitionTime + 0.5f)
+        {
+            t = elapsedTime / transitionTime;
+
+            goodExitLevelTransition.alpha = Mathf.Lerp(0f, 1f, t);
+
+            elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        enterLevelTransition.blocksRaycasts = true;
+        enterLevelTransition.interactable = true;
+
+        Debug.Log("Good Canvas Faded In");
 
         //Load Scene
         SceneManager.LoadScene(sceneIndex);
     }
 
-    IEnumerator LoadSceneBad(int sceneIndex)
+    IEnumerator LoadSceneBad(int sceneIndex, float transitionTime)
     {
-        //Play animation
-        badExitLevelTransition.SetTrigger("Start");
+        float elapsedTime = 0f;
+        float t = 0f;
 
-        //Wait
-        yield return new WaitForSecondsRealtime(transitionTime);
+        enterLevelTransition.blocksRaycasts = false;
+        enterLevelTransition.interactable = false;
+
+        while (elapsedTime < transitionTime + 0.5f)
+        {
+            t = elapsedTime / transitionTime;
+
+            badExitLevelTransition.alpha = Mathf.Lerp(0f, 1f, t);
+
+            elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        enterLevelTransition.blocksRaycasts = true;
+        enterLevelTransition.interactable = true;
+
+        Debug.Log("Bad Canvas Faded In");
 
         //Load Scene
         SceneManager.LoadScene(sceneIndex);
