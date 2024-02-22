94783f3dd1c86280494a043e7fa4b4930158f51c
Dylan Hall <DylHall592@gmail.com>
2024-02-22 01:01:47 -0500

    New Terrain Shader, also sfx for firefly

--- Assets/Scripts/Particles/FireflyController.cs
+++ Assets/Scripts/Particles/FireflyController.cs
@@ -10,18 +10,23 @@ public class FireflyController : MonoBehaviour
     [SerializeField] private List<Transform> Positions;
     private List<Transform> staticPositions;
     private int positionIndex=0;
+    private bool Moving = false;
 
     private void OnTriggerEnter(Collider other){
-        StartCoroutine(MoveToPoint(Positions[positionIndex++]));
+        if(!Moving)
+            StartCoroutine(MoveToPoint(Positions[positionIndex++]));
     }
 
     IEnumerator MoveToPoint(Transform goal){
+        Moving = true;
         Vector3 startPosition = transform.position;
         for (float t = 0f; t <= 1; t += Time.deltaTime / 2) {
 			transform.position = Vector3.Lerp (startPosition, goal.position, t);
 			yield return null;
         }
+        Moving = false;
         if(positionIndex+1 > Positions.Count)
             Destroy(this.gameObject);
     }
+
 }
