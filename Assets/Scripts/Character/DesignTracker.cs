92bd6fae979eb145621b5ee7df37b3baa744c50d
Dylan Hall <DylHall592@gmail.com>
2024-02-23 00:31:41 -0500

    Blendshape Adjustment Formula!
    Added secret max level shape

--- Assets/Scripts/Character/DesignTracker.cs
+++ Assets/Scripts/Character/DesignTracker.cs
@@ -13,6 +13,16 @@ public class DesignTracker : MonoBehaviour
     public UnityEngine.Color bodyColor;
     private bool blendCoroutineRunning = false;
     private List<int> UpdateTypes;
+    private int LevelCap = 15-1;                    //Blendshapes are based off starting at level 0, so this reflects the value of the max level minus one
+    private float sentienceWeight = 0;
+    private float primalWeight = 0;
+    private float vitalityWeight = 0;
+    private float speedWeight = 0;
+    [SerializeField] private bool PlaySFX = true;
+    private bool sfxEnabled=false;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip secretHighLevelSoundClip;
+    [SerializeField] private AudioClip growingSFX;
 
     private void Start()
     {
@@ -24,25 +34,72 @@ public class DesignTracker : MonoBehaviour
 
     public void UpdateBlendshape(int sentienceLevel, int primalLevel, int vitalityLevel, int speedLevel)
     {
-        //map the weight for Sentience
-        float sentienceWeight = sentienceLevel * 100f/10f;
-
-        //map the weight for Primal
-        float primalWeight = primalLevel * 100f/10f;
-
-        //map the weight for Vitality
-        float vitalityWeight = vitalityLevel * 100f/10f;
-
-        //map the weight for Speed
-        float speedWeight = speedLevel * 100f/10f;
+        sentienceLevel--;
+        primalLevel--;
+        vitalityLevel--;
+        speedLevel--;
+        
+        //CappingLevels, if any are over 15
+        if(sentienceLevel>LevelCap)
+            sentienceLevel=LevelCap;
+        if(primalLevel>LevelCap)
+            primalLevel=LevelCap;
+        if(vitalityLevel>LevelCap)
+            vitalityLevel=LevelCap;
+        if(speedLevel>LevelCap)
+            speedLevel=LevelCap;
 
         int totalLevel = sentienceLevel + primalLevel + vitalityLevel + speedLevel;
+        if(totalLevel<=LevelCap){
+            if (sfxEnabled == true) audioSource.PlayOneShot(growingSFX);
+            //map the weight for Sentience
+            sentienceWeight = sentienceLevel * 100f/LevelCap;
 
+            //map the weight for Primal
+            primalWeight = primalLevel * 100f/LevelCap;
+
+            //map the weight for Vitality
+            vitalityWeight = vitalityLevel * 100f/LevelCap;
+
+            //map the weight for Speed
+            speedWeight = speedLevel * 100f/LevelCap;
+        }
+        //Secret Ultimate Mode, if you're close to capping all 4 skills.
+        else if(totalLevel>=LevelCap*4){
+            if (sfxEnabled == true) audioSource.PlayOneShot(secretHighLevelSoundClip);
+            //map the weight for Sentience
+            sentienceWeight = sentienceLevel*sentienceLevel/(totalLevel-totalLevel*2) * 100f/LevelCap;
+
+            //map the weight for Primal
+            primalWeight = primalLevel*primalLevel/(totalLevel-totalLevel*2) * 100f/LevelCap;
+
+            //map the weight for Vitality
+            vitalityWeight = vitalityLevel*vitalityLevel/(totalLevel-totalLevel*2) * 100f/LevelCap;
+
+            //map the weight for Speed
+            speedWeight = speedLevel*speedLevel/(totalLevel-totalLevel*2) * 100f/LevelCap;
+        }
+
+        //If spore level total is above the level cap, adjust based on the level total. No cap can go past 100 ever.
+        else{
+            if (sfxEnabled == true) audioSource.PlayOneShot(growingSFX);
+            //map the weight for Sentience
+            sentienceWeight = sentienceLevel*sentienceLevel/totalLevel * 100f/LevelCap;
+
+            //map the weight for Primal
+            primalWeight = primalLevel*primalLevel/totalLevel * 100f/LevelCap;
+
+            //map the weight for Vitality
+            vitalityWeight = vitalityLevel*vitalityLevel/totalLevel * 100f/LevelCap;
+
+            //map the weight for Speed
+            speedWeight = speedLevel*speedLevel/totalLevel * 100f/LevelCap;
+        }
         if(blendCoroutineRunning) StopAllCoroutines();
         StartCoroutine(fadeBlend(sentienceWeight, primalWeight, vitalityWeight, speedWeight));
+        sfxEnabled = PlaySFX;
     }
 
-    //BUG: CANNOT LOWER BLENDSHAPE VALUES: ONLY RAISES THEM
     IEnumerator fadeBlend(float sentience, float primal, float vitality, float speed)
     {   
         blendCoroutineRunning =  true;
@@ -50,26 +107,58 @@ public class DesignTracker : MonoBehaviour
         float startingPrimal = skinnedMeshRenderer.GetBlendShapeWeight(3);
         float startingVitality = skinnedMeshRenderer.GetBlendShapeWeight(1);
         float startingSpeed = skinnedMeshRenderer.GetBlendShapeWeight(0);
-        while(skinnedMeshRenderer.GetBlendShapeWeight(2) < sentience)
-        {
-            skinnedMeshRenderer.SetBlendShapeWeight(2, skinnedMeshRenderer.GetBlendShapeWeight(2) + (sentience-startingSentience)/10f);
-            yield return new WaitForSeconds(0.02f);
-        }
-        while(skinnedMeshRenderer.GetBlendShapeWeight(3) < primal)
-        {
-            skinnedMeshRenderer.SetBlendShapeWeight(3, skinnedMeshRenderer.GetBlendShapeWeight(3) + (primal-startingPrimal)/10f);
-            yield return new WaitForSeconds(0.02f);
-        }
-        while(skinnedMeshRenderer.GetBlendShapeWeight(1) < vitality)
-        {
-            skinnedMeshRenderer.SetBlendShapeWeight(1, skinnedMeshRenderer.GetBlendShapeWeight(1) + (vitality-startingVitality)/10f);
-            yield return new WaitForSeconds(0.02f);
-        }
-        while(skinnedMeshRenderer.GetBlendShapeWeight(0) < speed)
-        {
-            skinnedMeshRenderer.SetBlendShapeWeight(0, skinnedMeshRenderer.GetBlendShapeWeight(0) + (speed-startingSpeed)/10f);
-            yield return new WaitForSeconds(0.02f);
-        }
+        //Adjusting Sentience
+        if(startingSentience<sentience)
+            while(skinnedMeshRenderer.GetBlendShapeWeight(2) < sentience)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(2, skinnedMeshRenderer.GetBlendShapeWeight(2) + (sentience-startingSentience)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        else
+            while(skinnedMeshRenderer.GetBlendShapeWeight(2) > sentience)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(2, skinnedMeshRenderer.GetBlendShapeWeight(2) + (sentience-startingSentience)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        //Adjusting Primal
+        if(startingPrimal<primal)
+            while(skinnedMeshRenderer.GetBlendShapeWeight(3) < primal)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(3, skinnedMeshRenderer.GetBlendShapeWeight(3) + (primal-startingPrimal)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        else
+            while(skinnedMeshRenderer.GetBlendShapeWeight(3) > primal)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(3, skinnedMeshRenderer.GetBlendShapeWeight(3) + (primal-startingPrimal)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        //Adjusting Vitality
+        if(startingVitality<vitality)
+            while(skinnedMeshRenderer.GetBlendShapeWeight(1) < vitality)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(1, skinnedMeshRenderer.GetBlendShapeWeight(1) + (vitality-startingVitality)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        else
+            while(skinnedMeshRenderer.GetBlendShapeWeight(1) > vitality)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(1, skinnedMeshRenderer.GetBlendShapeWeight(1) + (vitality-startingVitality)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        //Adjusting Speed
+        if(startingSpeed<speed)
+            while(skinnedMeshRenderer.GetBlendShapeWeight(0) < speed)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(0, skinnedMeshRenderer.GetBlendShapeWeight(0) + (speed-startingSpeed)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
+        else
+            while(skinnedMeshRenderer.GetBlendShapeWeight(0) > speed)
+            {
+                skinnedMeshRenderer.SetBlendShapeWeight(0, skinnedMeshRenderer.GetBlendShapeWeight(0) + (speed-startingSpeed)/10f);
+                yield return new WaitForSeconds(0.02f);
+            }
         blendCoroutineRunning = false;
     }
 
