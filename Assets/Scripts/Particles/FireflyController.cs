e7350fe62be130e2677c36bba6d3beebcdbcb76d
Dylan Hall <DylHall592@gmail.com>
2024-02-22 19:23:38 -0500

    HUGE UPDATE (Firefly moves different)

--- Assets/Scripts/Particles/FireflyController.cs
+++ Assets/Scripts/Particles/FireflyController.cs
@@ -11,17 +11,27 @@ public class FireflyController : MonoBehaviour
     private List<Transform> staticPositions;
     private int positionIndex=0;
     private bool Moving = false;
+    private bool QueueToMove = false;
 
-    private void OnTriggerEnter(Collider other){
-        if(!Moving)
+    private void Update(){
+        if(Moving==false && QueueToMove==true){
             StartCoroutine(MoveToPoint(Positions[positionIndex++]));
+        }
+    }
+    private void OnTriggerEnter(Collider other){
+        if(other.tag=="currentPlayer")
+            QueueToMove=true;
+    }
+    private void OnTriggerExit(Collider other){
+        if(other.tag=="currentPlayer")
+            QueueToMove=false;
     }
 
     IEnumerator MoveToPoint(Transform goal){
         Moving = true;
         Vector3 startPosition = transform.position;
-        for (float t = 0f; t <= 1; t += Time.deltaTime / 2) {
-			transform.position = Vector3.Lerp (startPosition, goal.position, t);
+        for (float t = 0f; t <= 1; t += Time.deltaTime / 3) {
+			transform.position = Vector3.Slerp(startPosition, goal.position, Mathf.SmoothStep(0, 1, t));
 			yield return null;
         }
         Moving = false;
