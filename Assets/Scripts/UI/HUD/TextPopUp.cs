432fd366be9fc6d0ba0192129231cb0678f534fe
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-02-25 02:04:45 -0500

    TextPopUp code fix

--- Assets/Scripts/UI/HUD/TextPopUp.cs
+++ Assets/Scripts/UI/HUD/TextPopUp.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using static System.TimeZoneInfo;
-using static UnityEditor.Rendering.CameraUI;
 
 public class TextPopUp : MonoBehaviour
 {
