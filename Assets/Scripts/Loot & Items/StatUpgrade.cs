c3f4d5b7aad3f77bde3221f4daf5ecbd184776ac
Perchan15 <97569540+Perchan15@users.noreply.github.com>
2024-03-04 19:58:24 -0500

    fixed repeat upgrade stats

--- Assets/Scripts/Loot & Items/StatUpgrade.cs
+++ Assets/Scripts/Loot & Items/StatUpgrade.cs
@@ -25,9 +25,9 @@ public class StatUpgrade : MonoBehaviour
         characterStats = player.GetComponent<CharacterStats>();
 
         List<int> statNumbers = new List<int> {0, 1, 2, 3};
-        int statNumber1 = Random.Range(0, statNumbers.Count);
+        int statNumber1 = statNumbers[Random.Range(0, statNumbers.Count)];
         statNumbers.Remove(statNumber1);
-        int statNumber2 = Random.Range(0, statNumbers.Count);
+        int statNumber2 = statNumbers[Random.Range(0, statNumbers.Count)];
 
         upgradeStat1 = NumberToStat(statNumber1);
         upgradeStat2 = NumberToStat(statNumber2);
