a58b46a5846632893e893bd7f13b839e32e6cb42
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-03-08 18:56:45 -0500

    CBO Scripts and Shaders

--- Assets/Scripts/Camera/CameraBasedOcclusion/CamOcclusionReceive.cs
+++ Assets/Scripts/Camera/CameraBasedOcclusion/CamOcclusionReceive.cs
@@ -4,15 +4,106 @@ using UnityEngine;
 
 public class CamOcclusionReceive : MonoBehaviour
 {
+    [SerializeField] private List<Renderer> meshRenderers;
+
+    private List<Material> materials = new List<Material>();
+
+    private Coroutine fadeOut;
+    private Coroutine fadeIn;
+
+    [HideInInspector] public bool isActivated = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(Renderer renderer in meshRenderers)
+        {
+            materials.Add(renderer.material);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartFadeOut()
     {
-        
+        //Debug.Log("ACTIVATING" + name, gameObject);
+
+        try
+        {
+            StopCoroutine(fadeIn);
+        }
+        catch
+        {
+
+        }
+
+        if(isActivated == false)
+        {
+            fadeOut = StartCoroutine(FadeOut());
+        }
+    }
+
+    public void StartFadeIn()
+    {
+        try
+        {
+            StopCoroutine(fadeOut);
+        }
+        catch
+        {
+
+        }
+
+        fadeIn = StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeOut()
+    {
+        isActivated = true;
+        float elapsedTime = 0f;
+        float t = 0f;
+        float time = 0.4f;
+
+        float startingOpacity = materials[0].GetFloat("_OpacityOverride");
+        float finalOpacity;
+
+        while (elapsedTime < time)
+        {
+            t = elapsedTime / time;
+
+            foreach(Material material in materials)
+            {
+                finalOpacity = Mathf.Lerp(1, 0, t);
+                material.SetFloat("_OpacityOverride", finalOpacity);
+            }
+            
+            elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        isActivated = false;
+        float elapsedTime = 0f;
+        float t = 0f;
+        float time = 0.3f;
+
+        float startingOpacity = materials[0].GetFloat("_OpacityOverride");
+        float finalOpacity;
+
+        while (elapsedTime < time)
+        {
+            t = elapsedTime / time;
+
+            foreach (Material material in materials)
+            {
+                finalOpacity = Mathf.Lerp(0, 1, t);
+                material.SetFloat("_OpacityOverride", finalOpacity);
+            }
+
+            elapsedTime += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Camera/CameraBasedOcclusion/CamOcclusionTransmit.cs
+++ Assets/Scripts/Camera/CameraBasedOcclusion/CamOcclusionTransmit.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CamOcclusionTransmit : MonoBehaviour
@@ -7,9 +9,14 @@ public class CamOcclusionTransmit : MonoBehaviour
     private Transform camTracker;
 
     private Ray occlusionCheckRay;
-    private RaycastHit[] hits = new RaycastHit[10];
+    private RaycastHit[] hits = new RaycastHit[100];
+    private int numOfHits;
+
+    private List<CamOcclusionReceive> latestReceiveScripts = new List<CamOcclusionReceive>();
+
     private Vector3 playerDirection;
     private float playerDistance;
+
     [SerializeField] private LayerMask occludedLayers;
 
     // Start is called before the first frame update
@@ -27,16 +34,72 @@ public class CamOcclusionTransmit : MonoBehaviour
 
         occlusionCheckRay = new Ray(transform.position, playerDirection);
 
-        if(Physics.Raycast(occlusionCheckRay, out RaycastHit hit, playerDistance, occludedLayers))
+        CastOcclusionRay();
+    }
+
+    void CastOcclusionRay()
+    {
+        numOfHits = Physics.SphereCastNonAlloc(occlusionCheckRay, 0.5f, hits, playerDistance, occludedLayers);
+
+
+
+        if (numOfHits > 0)
         {
-            Debug.DrawRay(occlusionCheckRay.origin, occlusionCheckRay.direction * playerDistance, Color.blue, 0f, false);
-            Debug.Log(hit.collider.gameObject.name, hit.collider.gameObject);
+            Debug.DrawRay(occlusionCheckRay.origin, occlusionCheckRay.direction * playerDistance, Color.blue * 2, 0f, false);
+            latestReceiveScripts = FindTheReceiver();
+            foreach(CamOcclusionReceive script in latestReceiveScripts)
+            {
+                script.StartFadeOut();
+            }
         }
         else
         {
-            Debug.DrawRay(occlusionCheckRay.origin, occlusionCheckRay.direction * playerDistance, Color.red, 0f, false);
+            Debug.DrawRay(occlusionCheckRay.origin, occlusionCheckRay.direction * playerDistance, Color.red * 2, 0f, false);
+            foreach (CamOcclusionReceive script in latestReceiveScripts)
+            {
+                script.StartFadeIn();
+            }
+        }
+    }
+
+    List<CamOcclusionReceive> FindTheReceiver()
+    {
+        GameObject searchObject;
+        List<GameObject> objectsWithScript = new List<GameObject>();
+        List<CamOcclusionReceive> receiveScripts = new List<CamOcclusionReceive>();
+
+        //Add objects to the list that contain the Receive Script (This WILL add duplicates)
+        for(int i = 0; i < numOfHits; i++)
+        {
+            searchObject = hits[i].transform.gameObject;
+
+            if (searchObject.GetComponent<CamOcclusionReceive>() != null)
+            {
+                objectsWithScript.Add(searchObject);
+            }
+
+            while (searchObject.transform.parent != null)
+            {
+                if (searchObject.GetComponent<CamOcclusionReceive>() != null)
+                {
+                    objectsWithScript.Add(searchObject);
+                    break;
+                }
+
+                searchObject = searchObject.transform.parent.gameObject;
+            }
         }
 
-        
+        //Remove the Duplicates
+        objectsWithScript = objectsWithScript.Distinct().ToList();
+
+        //Get the references to the Receive Scripts
+        foreach(GameObject singleObject in objectsWithScript)
+        {
+            receiveScripts.Add(singleObject.GetComponent<CamOcclusionReceive>());
+            //Debug.Log(singleObject.name, singleObject);
+        }
+
+        return receiveScripts;
     }
 }
