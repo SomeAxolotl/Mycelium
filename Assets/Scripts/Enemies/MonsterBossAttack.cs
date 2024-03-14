81d407e7a63003a7e4cee430ce3afe660750e51f
Shawn <lilpez2000@gmail.com>
2024-03-14 16:42:15 -0400

    Boss and Random Attacks
    Adds the boss tail attack and randomizes the random attacks correctly

--- Assets/Scripts/Enemies/MonsterBossAttack.cs
+++ Assets/Scripts/Enemies/MonsterBossAttack.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 
 public class MonsterBossAttack : MonoBehaviour
 {
-    private MonsterBossMovement bossMovement;
+    private TempMovement bossMovement;
     [SerializeField] public float attractionForce = 100.0f;
     [SerializeField] public float attractionRadius = 20.0f;
     [SerializeField] public float pullInterval = 5.0f;
@@ -17,6 +17,11 @@ public class MonsterBossAttack : MonoBehaviour
     public bool canAttack = true;
     public bool isAttacking = false;
     public bool playerDamaged = false;
+    private bool isTailAttacking = false;
+    private bool isSlamAttacking = false;
+    private bool isSwipeAttacking = false;
+    private bool tailAttackOnCooldown = false;
+    private bool isRandomOnCooldown = false;
     [SerializeField] private float attackInterval = 2f;
     private float attackWindupTime = .5f;
     private float resetAttack;
@@ -27,17 +32,23 @@ public class MonsterBossAttack : MonoBehaviour
     List<GameObject> playerHit = new List<GameObject>();
     private Transform player;
     private Rigidbody rb;
+    [SerializeField] private GameObject bossTail;
     PlayerController playerController;
     [SerializeField] private float tailAttackAnimationDuration = 1.5f;
     [SerializeField] private float swipeAttackAnimationDuration = 1.0f;
     [SerializeField] private float slamAttackAnimationDuration = 2.0f;
+    [SerializeField] private float tailAttackDamage = 30f;
+    [SerializeField] private float swipeAttackDamage = 25f;
+    [SerializeField] private float slamAttackDamage = 35f;
+    [SerializeField] private float tailKnockback = 70f;
+
 
     // Start is called before the first frame update
     void Start()
     {
         PlayerController playerController = GetComponent<PlayerController>();
-        bossMovement = GetComponent<MonsterBossMovement>();
-        attack = this.Attack();
+        bossMovement = GetComponent<TempMovement>();
+        attack = this.Attack(damage);
         animator = GetComponent<Animator>();
         player = GameObject.FindWithTag("currentPlayer").transform;
         rb = GetComponent<Rigidbody>();
@@ -46,6 +57,7 @@ public class MonsterBossAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float dstToPlayer = Vector3.Distance(transform.position, player.position);
         elapsedTime += Time.deltaTime;
         if (elapsedTime >= pullInterval)
         {
@@ -56,12 +68,21 @@ public class MonsterBossAttack : MonoBehaviour
         {
             CancelAttack();
         }
-        else
+        else if (bossMovement.playerSeen && canAttack && dstToPlayer <= 20f)
         {
-            if (bossMovement.playerSeen && canAttack)
+            isTailAttacking = false;
+            if (!isRandomOnCooldown)
             {
-                StartCoroutine(Attack());
                 StartCoroutine(TriggerRandomAttacksWithDelay());
+            }
+        }
+        else if (dstToPlayer > 20f && bossMovement.playerSeen && canAttack)
+        {
+
+            if (!isTailAttacking && !tailAttackOnCooldown)
+            {
+                isTailAttacking = true;
+                StartCoroutine(TailAttack());
             }
         }
         if (isAttacking)
@@ -73,7 +94,7 @@ public class MonsterBossAttack : MonoBehaviour
             resetAttack = 0f;
         }
     }
-    IEnumerator Attack()
+    private IEnumerator Attack(float damage)
     {
         canAttack = false;
         isAttacking = true;
@@ -106,7 +127,7 @@ public class MonsterBossAttack : MonoBehaviour
     public void CancelAttack()
     {
         StopAllCoroutines();
-        attack = Attack();
+        attack = Attack(damage);
         isAttacking = false;
         playerDamaged = false;
         playerHit.Clear();
@@ -118,6 +139,21 @@ public class MonsterBossAttack : MonoBehaviour
     {
         if (other.gameObject.tag == "currentPlayer" && other.gameObject.GetComponentInParent<PlayerController>().isInvincible == false && !playerHit.Contains(other.gameObject) && isAttacking)
         {
+            if (isTailAttacking && !isSlamAttacking || !isSwipeAttacking)
+            {
+                damage = tailAttackDamage;
+                knockbackForce = 300f;
+            }
+            if (isSlamAttacking && !isSwipeAttacking)
+            {
+                damage = slamAttackDamage;
+                knockbackForce = 100f;
+            }
+            if (isSwipeAttacking && !isSlamAttacking)
+            {
+                damage = swipeAttackDamage;
+                knockbackForce = 200f;
+            }
             playerDamaged = false;
             other.gameObject.GetComponentInParent<PlayerHealth>().PlayerTakeDamage(damage);
             other.gameObject.GetComponentInParent<PlayerController>().Knockback(this.gameObject, knockbackForce);
@@ -163,50 +199,68 @@ public class MonsterBossAttack : MonoBehaviour
     }
     private IEnumerator TailAttack()
     {
+        tailAttackOnCooldown = true;
         //animator.SetTrigger("TailAttack");
         Debug.Log("TAIL ATTACK!");
-        yield return new WaitForSeconds(tailAttackAnimationDuration);
+        float customDuration = 3.0f; // Replace with your custom duration
+        Debug.Log("Waiting for " + customDuration + " seconds...");
+        yield return new WaitForSeconds(customDuration);
 
+        Debug.Log("Tail Attack Animation Finished!");
+        Vector3 playerPosition = player.position;
+        Vector3 spawnPosition = playerPosition + transform.forward * 1f;
+        GameObject bossTailInstance = Instantiate(bossTail, spawnPosition, Quaternion.identity, transform);
+        StartCoroutine(Attack(tailAttackDamage));
+        knockbackForce = tailKnockback;
         ApplyKnockbackToPlayer();
+
+        yield return new WaitForSeconds(5f);
+        Debug.Log("Tail Attack off cooldown!");
+        Destroy(bossTailInstance);
+        tailAttackOnCooldown = false;
+        isTailAttacking = false;
     }
 
     private IEnumerator SwipeAttack()
     {
+        isSwipeAttacking = true;
         //animator.SetTrigger("SwipeAttack");
         Debug.Log("SWIPE ATTACK!");
         yield return new WaitForSeconds(swipeAttackAnimationDuration);
+        StartCoroutine(Attack(swipeAttackDamage));
 
         ApplyKnockbackToPlayer();
+        isSwipeAttacking = false;
     }
 
     private IEnumerator SlamAttack()
     {
+        isSlamAttacking = true;
         //animator.SetTrigger("SlamAttack");
         Debug.Log("SLAM ATTACK!");
         yield return new WaitForSeconds(slamAttackAnimationDuration);
+        StartCoroutine(Attack(slamAttackDamage));
 
         ApplyKnockbackToPlayer();
+        isSlamAttacking = false;
     }
 
     private void ApplyKnockbackToPlayer()
     {
         // Apply knockback effect to player here
-        // Example: playerController.Knockback(this.gameObject, knockbackForce);
+        // playerController.Knockback(this.gameObject, knockbackForce);
     }
 
     private void TriggerRandomAttack()
     {
-        int randomAttack = Random.Range(0, 3);
+        int randomAttack = Random.Range(0, 2);
 
         switch (randomAttack)
         {
             case 0:
-                StartCoroutine(TailAttack());
-                break;
-            case 1:
                 StartCoroutine(SwipeAttack());
                 break;
-            case 2:
+            case 1:
                 StartCoroutine(SlamAttack());
                 break;
             default:
@@ -218,10 +272,20 @@ public class MonsterBossAttack : MonoBehaviour
     {
         while (true)
         {
-            TriggerRandomAttack();
-
-            yield return new WaitForSeconds(5.0f);
+            if (!isRandomOnCooldown)
+            {
+                TriggerRandomAttack();
+                isRandomOnCooldown = true;
+                float customDelayDuration = 5.0f; // Replace with your custom duration
+                yield return new WaitForSeconds(customDelayDuration);
+                isRandomOnCooldown = false;
+            }
+            else
+            {
+                yield return null;
+            }
         }
     }
 }
+
 
