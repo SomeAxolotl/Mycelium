9674d5d6f054a0457ae585ad82fb9bd0b8dd385a
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-03-17 13:39:39 -0400

    CarcassClouds

--- Assets/Scripts/Skybox/FollowThePlayer.cs
+++ Assets/Scripts/Skybox/FollowThePlayer.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour
+public class FollowThePlayer : MonoBehaviour
 {
-    [SerializeField] Transform followThis;
+    private Transform followThis;
 
     // Start is called before the first frame update
     void Start()
