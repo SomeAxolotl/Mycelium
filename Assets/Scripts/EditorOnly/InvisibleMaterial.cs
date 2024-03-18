2f43f5ccbf8e9f6df9991cf8401765b670292c95
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-03-17 20:45:28 -0400

    Custom inspector for InvisibleMaterial Script

--- Assets/Scripts/EditorOnly/InvisibleMaterial.cs
+++ Assets/Scripts/EditorOnly/InvisibleMaterial.cs
@@ -1,14 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class InvisibleMaterial : MonoBehaviour
-{
+{    private MeshRenderer thisMeshRenderer;
+
     void Start()
     {
+        thisMeshRenderer = GetComponent<MeshRenderer>();
+
         if (Application.isPlaying)
         {
-            GetComponent<MeshRenderer>().enabled = false;
+            thisMeshRenderer.enabled = false;
+        }
+    }
+
+    public void ToggleAllVisibility()
+    {
+        if(Application.isPlaying == false)
+        {
+            return;
+        }
+
+        MeshRenderer mr;
+
+        foreach(GameObject singleObject in GameObject.FindGameObjectsWithTag("InvisibleWalls"))
+        {
+            mr = singleObject.GetComponent<MeshRenderer>();
+            mr.enabled = !mr.enabled;
         }
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(InvisibleMaterial))]
+class InvisibleMaterialEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //Setup Stuff
+        base.OnInspectorGUI();
+
+        var invisibleMaterial = (InvisibleMaterial)target;
+        if (invisibleMaterial == null) return;
+
+        //Actual Stuff
+        if (GUILayout.Button("Toggle Visibility For All Walls"))
+        {
+            invisibleMaterial.ToggleAllVisibility();
+        }
+
+        
+    }
+}
+#endif
