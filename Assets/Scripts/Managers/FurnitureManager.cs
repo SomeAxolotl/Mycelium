d14278b7f6967d0c3b7ca1c4d2a3ff747d19b64f
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-04-15 16:57:35 -0400

    furnituremanager singleton

--- Assets/Scripts/Managers/FurnitureManager.cs
+++ Assets/Scripts/Managers/FurnitureManager.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class FurnitureManager : MonoBehaviour
 {
+    public static FurnitureManager Instance;
+
     public bool bedIsUnlocked;
     public bool drumIsUnlocked;
     public bool chairIsUnlocked;
     public bool fireflyIsUnlocked;
     public bool gameboardIsUnlocked;
     public bool fireIsUnlocked;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
 
     public bool FurnitureIsUnlocked(string furnitureName)
     {
