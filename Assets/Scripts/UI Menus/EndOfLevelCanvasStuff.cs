99a64cd132775e5ff58c4abc3383a2257bccf75d
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-04-15 04:11:38 -0400

    Checkpoint UI

--- Assets/Scripts/UI Menus/EndOfLevelCanvasStuff.cs
+++ Assets/Scripts/UI Menus/EndOfLevelCanvasStuff.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,8 +11,10 @@ public class EndOfLevelCanvasStuff : MonoBehaviour
 
     [SerializeField] CanvasGroup blackCanvas;
     [SerializeField] CanvasGroup endOfLevelCanvas;
+    [SerializeField] TMP_Text endOfLevelText;
 
     private LevelEnd levelEndScript;
+    private string defaultText;
 
     private void Awake()
     {
@@ -25,17 +28,39 @@ public class EndOfLevelCanvasStuff : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        defaultText = endOfLevelText.text;
+    }
+
     public void StartEndOfLevel(LevelEnd end)
     {
         levelEndScript = end;
+
+        if(levelEndScript.isCheckpoint == true)
+        {
+            endOfLevelText.text = "It looks like this leads back to The Carcass. Would you like to continue your adventure or return home?";
+        }
+        else
+        {
+            endOfLevelText.text = defaultText;
+        }
+
         StartCoroutine(EndOfLevel());
     }
 
     public void ContinueAdventure()
     {
-        EventSystem.current.SetSelectedGameObject(null);
-        Time.timeScale = 1;
-        levelEndScript.GoToNextLevel();
+        if (levelEndScript.isCheckpoint == true)
+        {
+            StartCoroutine(EndOfCheckpoint());
+        }
+        else
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            Time.timeScale = 1;
+            levelEndScript.GoToNextLevel();
+        }
     }
 
     public void FinishAdventure()
@@ -49,12 +74,27 @@ public class EndOfLevelCanvasStuff : MonoBehaviour
     {
         yield return StartCoroutine(FadeIn(blackCanvas, 0.5f));
 
-        yield return new WaitForSecondsRealtime(NotificationManager.Instance.totalDuration);
+        if (levelEndScript.isCheckpoint == false)
+        {
+            yield return new WaitForSecondsRealtime(NotificationManager.Instance.totalDuration);
+        }
 
         GameObject.Find("ContinueButton").GetComponent<Button>().Select();
         StartCoroutine(FadeIn(endOfLevelCanvas, 0.5f));
 
         Time.timeScale = 0;
+    }
+
+    IEnumerator EndOfCheckpoint()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        StartCoroutine(FadeOut(blackCanvas, 0.5f));
+        yield return StartCoroutine(FadeOut(endOfLevelCanvas, 0.5f));
+
+        levelEndScript.GoBackToGameplay();
+
+        Time.timeScale = 1;
     }
 
     IEnumerator FadeIn(CanvasGroup canvasGroup, float transitionTime)
