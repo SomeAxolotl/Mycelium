f3c5f8f9c1fd0246621b6aadfd4faec084288ef2
Shawn <lilpez2000@gmail.com>
2024-05-14 00:03:31 -0400

    Returnable Enemy Projectiles
    Hitting an enemy projectile will now return it to the enemy for the amount of damage it would have done to the player.

--- Assets/Scripts/Enemies/RangedEnemyProjectile.cs
+++ Assets/Scripts/Enemies/RangedEnemyProjectile.cs
@@ -6,15 +6,24 @@ public class RangedEnemyProjectile : MonoBehaviour
 {
     [SerializeField] private float damage = 10f;
     [SerializeField] private GameObject ExplosionVFX;
+    [SerializeField] private Material negativeVelocityMaterial;
+    [SerializeField] private LayerMask collidableLayers;
     float gravityForce = 9.3f;
     Vector3 gravity;
     Rigidbody rb;
+    Renderer rend;
+    bool enemyCollisionOccurred = false;
+    public CharacterStats primalLevel;
+    Spineshot spineshot;
 
     private void Start()
     {
+        primalLevel = GetComponent<CharacterStats>();
         Destroy(gameObject, 5f);
         gravity = new Vector3(0f, gravityForce, 0f);
         rb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        spineshot = GameObject.FindWithTag("currentPlayer").GetComponentInChildren<Spineshot>();
     }
     private void FixedUpdate()
     {
@@ -32,5 +41,26 @@ public class RangedEnemyProjectile : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else if (collision.gameObject.layer == 10)
+        {
+            gameObject.tag = "ReversedProjectile";
+            enemyCollisionOccurred = false;
+            rb.velocity = -rb.velocity * 2.5f;
+
+            rend.material = negativeVelocityMaterial;
+        }
+        if (collision.gameObject.tag == "Enemy" && this.gameObject.tag == "ReversedProjectile")
+        {
+            enemyCollisionOccurred = true;
+            if (collision.GetComponent<EnemyHealth>() != null)
+            {
+
+                collision.GetComponentInParent<EnemyHealth>().EnemyTakeDamage(damage * GlobalData.currentLoop);
+            }
+            Instantiate(ExplosionVFX, transform.position, transform.rotation);
+            Destroy(gameObject);
+
+        }
+
     }
 }
