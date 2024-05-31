8e3e45cb16046d19fa7ebb919ba11cc1b602ce84
griffinbob1 <98678695+griffinbob1@users.noreply.github.com>
2024-05-30 20:15:48 -0400

    Pseudo Random Caches
    +Caches now have specific chances for weapons, stat changes, and resources
    +When a cache does not get a weapon, it has increased chance to get a weapon next cache

--- Assets/Scripts/Loot & Items/LootCache.cs
+++ Assets/Scripts/Loot & Items/LootCache.cs
@@ -11,8 +11,13 @@ using System;
 public class LootCache : MonoBehaviour, IInteractable
 {
     float distance;
-    [SerializeField][Tooltip("The Loot Table Object used for this Cache;")] private LootTable lootTable;
-    [SerializeField][Tooltip("Legacy. Used only for reference.")] private List<GameObject> possibleDrops;
+    [SerializeField][Tooltip("Loot table of all weapons")] private LootTable weaponTable;
+    [SerializeField][Tooltip("Chance to pull a weapon from cache")] private float weaponChance;
+    [SerializeField][Tooltip("Loot table of all stats")] private LootTable statTable;
+    [SerializeField][Tooltip("Chance to pull a stat from cache")] private float statChance;
+    [SerializeField][Tooltip("Loot table of all resources")] private LootTable resourceTable;
+    [SerializeField][Tooltip("Chance to pull a resource from cache")] private float resourceChance;
+    [HideInInspector][Tooltip("Legacy. Used only for reference.")] private List<GameObject> possibleDrops;
     [SerializeField] private int nutrientMin = 50;
     [SerializeField] private int nutrientMax = 200;
     GameObject player;
@@ -66,19 +71,25 @@ public class LootCache : MonoBehaviour, IInteractable
 
     public void GetLoot()
     {
-        List<Loot> possibleItems = new List<Loot>();
-        do
-        {
-            int randomIndex = UnityEngine.Random.Range(1,101);
-            foreach(Loot item in lootTable.loots)
-            {
-                if(randomIndex <= item.DropChance)
-                {
-                    possibleItems.Add(item);
-                }
-            }
-        } while (possibleItems.Count == 0);
-        Loot droppedItem = possibleItems[UnityEngine.Random.Range(0,possibleItems.Count)];
+        float totalChance = (weaponChance + statChance + resourceChance) + (RandomManager.bonusWeaponChance + RandomManager.bonusStatChance + RandomManager.bonusResourceChance);
+        float randomValue = UnityEngine.Random.Range(0f, totalChance);
+        Loot droppedItem;
+        if(randomValue <= weaponChance + RandomManager.bonusWeaponChance){
+            droppedItem = PickByWeight(weaponTable.loots);
+            RandomManager.bonusWeaponChance = 0;
+            RandomManager.bonusStatChance += statChance * 0.1f;
+            RandomManager.bonusResourceChance += resourceChance * 0.1f;
+        }else if(randomValue - weaponChance <= statChance + RandomManager.bonusStatChance){
+            droppedItem = PickByWeight(statTable.loots);
+            RandomManager.bonusWeaponChance += weaponChance * 0.1f;
+            RandomManager.bonusStatChance = 0;
+            RandomManager.bonusResourceChance += resourceChance * 0.1f;
+        }else{
+            droppedItem = PickByWeight(resourceTable.loots);
+            RandomManager.bonusWeaponChance += weaponChance * 10.1f;
+            RandomManager.bonusStatChance += statChance * 0.1f;
+            RandomManager.bonusResourceChance = 0;
+        }
         Instantiate(droppedItem.LootPrefab, new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z), Quaternion.identity);
         
         int randomNutrientValue = UnityEngine.Random.Range(nutrientMin, nutrientMax);
@@ -88,5 +99,21 @@ public class LootCache : MonoBehaviour, IInteractable
         }
         //GameObject.Find("NutrientCounter").GetComponent<NutrientTracker>().AddNutrients(randomNutrientValue);
         ParticleManager.Instance.SpawnParticleFlurry("NutrientParticles", randomNutrientValue, 0.1f, this.gameObject.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+    }
+
+    private Loot PickByWeight(List<Loot> loots){
+        float totalChance = 0;
+        foreach(Loot currLoot in loots){
+            totalChance += currLoot.DropChance;
+            //Debug.Log(currLoot + ":  " + currLoot.DropChance);
+        }
+        float randomValue = UnityEngine.Random.Range(0f, totalChance);
+        foreach(Loot currLoot in loots){
+            randomValue -= currLoot.DropChance;
+            if(randomValue <= 0){
+                return currLoot;
+            }
+        }
+        return loots[UnityEngine.Random.Range(0, loots.Count)];
     }
 }
---
+++ Assets/Scripts/Managers/RandomManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RandomManager
+{
+    public static float bonusWeaponChance = 0;
+    public static float bonusStatChance = 0;
+    public static float bonusResourceChance = 0;
+}
