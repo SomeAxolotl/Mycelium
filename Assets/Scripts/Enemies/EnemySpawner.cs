aab3af786fd4adb3b42a5ad0c2ac8d70d55c6a54
Perchan15 <97569540+Perchan15@users.noreply.github.com>
2024-06-02 08:12:08 -0400

    spawner tweaks

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class EnemySpawner : MonoBehaviour
-{
+{   
     [SerializeField]List<EnemySpawn> possibleEnemies = new List<EnemySpawn>();
     List<GameObject> spawnedEnemies = new List<GameObject>();
+
     enum ActivateTypes
     {
         Start,
@@ -19,12 +24,35 @@ public class EnemySpawner : MonoBehaviour
         Interval,
         Indefinite,
     }
-
-    [SerializeField] float spawnRange = 5f;
     [SerializeField] SpawnTypes spawnType = SpawnTypes.All;
+
+    [SerializeField][Tooltip("How far from the center of the spawner enemies will poop out")] float spawnRadius = 5f;
     [SerializeField] int maxEnemies = 5;
     [SerializeField] float spawnDelay = 0f;
     [SerializeField][Tooltip("Only used with Interval and Indefinite ActivateTypes")] float intervalAmount = 3f; 
+    
+    float popDuration = 0.25f;
+
+    void Start()
+    {
+        if(activateType == ActivateTypes.Start)
+        {
+            Activate();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "currentPlayer")
+        {
+            if(activateType == ActivateTypes.TriggerEnter)
+            {
+                Activate();
+            }
+        }
+    
+    }
+    
     public void Activate()
     {
         StartCoroutine(ActivateCoroutine());
@@ -68,33 +96,23 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(intervalAmount);
-
-            List<GameObject> enemiesToRemove = new List<GameObject>();
-            foreach (GameObject spawnedEnemy in spawnedEnemies)
-            {
-                if (!spawnedEnemy.activeSelf)
-                {
-                    enemiesToRemove.Add(spawnedEnemy);
-                }
-            }
-            foreach (GameObject enemyToRemove in enemiesToRemove)
-            {
-                spawnedEnemies.Remove(enemyToRemove);
-            }
-
+            spawnedEnemies.RemoveAll(spawnedEnemy => !spawnedEnemy.activeSelf);
 
             if (spawnedEnemies.Count < maxEnemies)
             {
-               SpawnEnemy();
-                //StartCoroutine(PopEnemy(spawnedEnemy));
+                SpawnEnemy();
             }
+
+            yield return new WaitForSeconds(intervalAmount);
         }
     }
 
-    GameObject SpawnEnemy()
+    public GameObject SpawnEnemy()
     {
-        if (possibleEnemies.Count == 0) return null;
+        if (possibleEnemies.Count == 0)
+        {
+            Debug.LogError(this.gameObject + " has no possibleEnemies");
+        }
 
         float totalWeight = 0f;
         foreach (EnemySpawn enemySpawn in possibleEnemies)
@@ -108,37 +126,67 @@ public class EnemySpawner : MonoBehaviour
         {
             if (randomValue < enemySpawn.weight)
             {
-                GameObject spawnedEnemy = Instantiate(enemySpawn.prefab, new Vector3(transform.position.x + UnityEngine.Random.Range(-spawnRange, spawnRange), transform.position.y, transform.position.z + UnityEngine.Random.Range(-spawnRange, spawnRange)), Quaternion.identity);
-                spawnedEnemies.Add((spawnedEnemy));
-                if(spawnedEnemies.Count >= maxEnemies && spawnType != SpawnTypes.Indefinite)
+                if (enemySpawn.prefab != null)
                 {
-                    Destroy(gameObject);
+                    GameObject spawnedEnemy = Instantiate(enemySpawn.prefab, new Vector3(transform.position.x + UnityEngine.Random.Range(-spawnRadius, spawnRadius), transform.position.y, transform.position.z + UnityEngine.Random.Range(-spawnRadius, spawnRadius)), Quaternion.identity);
+                    spawnedEnemies.Add(spawnedEnemy);
+
+                    Coroutine popCoroutine = StartCoroutine(PopEnemy(spawnedEnemy));
+
+                    if(spawnedEnemies.Count >= maxEnemies && spawnType != SpawnTypes.Indefinite)
+                    {
+                        StartCoroutine(DestroyAfterPopping(popCoroutine));
+                    }
+
+                    return spawnedEnemy;
                 }
-                return spawnedEnemy;
+                else
+                {
+                    Debug.LogError("Enemy spawn " + enemySpawn + " has no assigned prefab (" + this.gameObject + ")");
+
+                    #if UNITY_EDITOR
+                    EditorGUIUtility.PingObject(this.gameObject);
+                    #endif
+
+                    return null;
+                }
             }
-            randomValue -= enemySpawn.weight;
+            else
+            {
+                randomValue -= enemySpawn.weight;
+            }
         }
+        Debug.LogError(this.gameObject + " had a total weight of " + totalWeight + " but couldn't find an enemy prefab. Returning null.");
         return null;
     }
 
-    void OnTriggerEnter(Collider other)
+    IEnumerator PopEnemy(GameObject enemyToPop)
     {
-        if(other.gameObject.tag == "currentPlayer")
+        Vector3 originalEnemyScale = enemyToPop.transform.localScale;
+        enemyToPop.transform.localScale = Vector3.zero;     
+
+        float popCounter = 0f;
+        while (popCounter < popDuration)
         {
-            if(activateType == ActivateTypes.TriggerEnter)
-            {
-                Activate();
-            }
+            float t = DylanTree.EaseOutQuart(popCounter / popDuration);
+
+            popCounter += Time.deltaTime;
+
+            enemyToPop.transform.localScale = Vector3.Lerp(Vector3.zero, originalEnemyScale, t);
+
+            yield return null;
         }
-    
+
+        enemyToPop.transform.localScale = originalEnemyScale;
     }
-    void Start()
+
+    IEnumerator DestroyAfterPopping(Coroutine popCoroutine)
     {
-        if(activateType == ActivateTypes.Start)
-        {
-            Activate();
-        }
+        yield return popCoroutine;
+
+        Destroy(this.gameObject);
     }
+
     [System.Serializable]
     class EnemySpawn
     {
