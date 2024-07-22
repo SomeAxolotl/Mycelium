a4e78a785068871640911429f9b65b8541025d5e
griffinbob1 <98678695+griffinbob1@users.noreply.github.com>
2024-07-22 15:10:51 -0400

    Loop Particle Ranks
    +Particles change based on the amount of loops cleared capping at 9

--- Assets/Scripts/Particles/RateValue.cs
+++ Assets/Scripts/Particles/RateValue.cs
@@ -12,14 +12,30 @@ public class RateValue : MonoBehaviour
     }
     set{
         rateMult = value;
-        if(savedParticles != null){
-            var emmision = savedParticles.emission;
-            emmision.rateOverTime = (rateMult * baseRate);
-        }
+        PickColorAndRate();
     }
     }
     public float baseRate = 1;
     [SerializeField] private ParticleSystem savedParticles;
+
+    private void PickColorAndRate(){
+        //Bronze tier
+        if(savedParticles == null){return;}
+        var emmision = savedParticles.emission;
+        if(rateMult <= 0){emmision.rateOverTime = 0; return;}
+        if(rateMult <= 4){
+            emmision.rateOverTime = (rateMult * baseRate);
+            savedParticles.startColor = new Color(0.69f, 0.55f, 0.34f, 1f);
+            return;
+        }
+        if(rateMult <= 8){
+            emmision.rateOverTime = ((rateMult - 4) * baseRate);
+            savedParticles.startColor = new Color(0.66f, 0.66f, 0.66f, 1f);
+            return;
+        }
+        emmision.rateOverTime = 3;
+        savedParticles.startColor = new Color(0.83f, 0.69f, 0.22f, 1f);
+    }
 
     private void Awake(){
         if(savedParticles == null){
