0d9d27d495430c9d82dc5be1d92422fe6ef368f9
fusionbolt18 <fusionbolt.rp@gmail.com>
2024-07-22 00:41:14 -0400

    fixed intro panning thing

--- Assets/Scripts/Camera/CameraCulldistance.cs
+++ Assets/Scripts/Camera/CameraCulldistance.cs
@@ -14,25 +14,6 @@ public class CameraCulldistance : MonoBehaviour
     void Start()
     {
         RefreshRenderDistance();
-
-        //this has nothing to do with culling its actually just to adjust the x position of the fancy cinemachine camera thing. -ryan
-        if(SceneManager.GetActiveScene().name == "New Tutorial")
-        {
-            StartCoroutine(LookLeft(GetComponent<CinemachineFreeLook>()));
-        }
-    }
-
-    IEnumerator LookLeft(CinemachineFreeLook freeLook)
-    {
-        GetComponent<CinemachineInputProvider>().enabled = false;
-
-        for(int i = 0; i < 200; i++)
-        {
-            freeLook.m_XAxis.Value += 0.6f;
-            yield return null;
-        }
-
-        GetComponent<CinemachineInputProvider>().enabled = true;
     }
 
     public void SetRenderDistance(float newRenderDistance)
