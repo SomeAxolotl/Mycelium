501e424d5aefb06760f7131065d17507ca8538e1
Perchan15 <97569540+Perchan15@users.noreply.github.com>
2024-07-25 05:39:34 -0400

    fairy ring sfx

--- Assets/Scripts/Skills/Species/FairyRing.cs
+++ Assets/Scripts/Skills/Species/FairyRing.cs
@@ -15,6 +15,7 @@ public class FairyRing : Skill
     {
         GameObject fairyRingInstance = Instantiate(fairyRingPrefab, (player.transform.position + new Vector3(0f, 0.2f, 0f)), Quaternion.identity);
         ParticleManager.Instance.SpawnParticles("FairyRingBackUp", player.transform.position, Quaternion.Euler(-90,0,0));
+        SoundEffectManager.Instance.PlaySound("Projectile", player.transform.position, volumeModifier: 0.7f, pitchMultiplier: 0.6f);
         fairyRingInstance.GetComponent<FairyRingPlacement>().damage = finalSkillValue;
     }
 }
